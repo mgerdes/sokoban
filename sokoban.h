6f7676dce28e2810c9b1c6cc4220be69fb83d94a
Michael Gerdes <mgerdes11@gmail.com>
2015-11-04 20:03:06 -0500

    Changed up some of the colors of the cubes

--- sokoban.h
+++ sokoban.h
@@ -1,9 +1,9 @@
-static const GLfloat player_color[] = {1.0, 1.0, 1.0};
+static const GLfloat player_color[] = {0.1, 0.1, 0.1};
 static const GLfloat wall_color[] = {0.2, 0.2, 0.7};
 static const GLfloat crate_color[] = {0.5, 0.0, 0.5};
 static const GLfloat crate_on_dest_color[] = {0.6, 0.0, 0.6};
 static const GLfloat floor_color[] = {0.8, 0.8, 0.8};
-static const GLfloat dest_color[] = {0.0, 0.6, 0.0};
+static const GLfloat dest_color[] = {0.4, 0.9, 0.4};
 
 static GLfloat cube_normals[] = {
     0.0, -1.0, 0.0,
@@ -153,7 +153,7 @@ const char* vertex_shader_source =
     "uniform sampler2D basic_texture;"
     "void main () {"
     "   texture_coords = vertex_texture;"
-    "   vec3 light_position = vec3(10.0, 10.0, -10.0);"
+    "   vec3 light_position = vec3(10.0, 10.0, -5.0);"
     "   vec3 L = normalize(light_position - vertex_position);"
     "   float dot_prod = max(dot(L, vertex_normal), 0.3);"
     "   color = dot_prod * vertex_color;"
